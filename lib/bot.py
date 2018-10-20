6513b53fe64e0c98a892fd9dba13f5f8f638f008
lunhanig <hacker@dev.org.br>
2018-10-20 22:41:09 +0000

    melhoradas classes e maneira de implementar bot com abstração e plugins

--- lib/bot.py
+++ lib/bot.py
@@ -1,80 +1,63 @@
 # -*- coding: utf-8 -*-
 
-import time, os, json
-from elasticsearch import Elasticsearch
-from webwhatsapi import WhatsAPIDriver
 
+from webwhatsapi import WhatsAPIDriver
+from git import Repo
+import os
+
+# Abstract class to implement WebWhtasapp-Wrapper bots
 # Author : Luis Fagundes
 # Co-author: Guilherme Lunhani
-class Bot:
+class Bot(object):
 
-  def __init__(self, username, profile, config, logfile, headless=True):
-    # Initialize Driver
-    # See for selenium
-    try:
-      os.environ["SELENIUM"]
+  def __init__(self, **kwargs):
+    self.attributes = []
+    self.properties = {}
+    # Default cache folder must exist!
+    p = os.path.abspath(os.path.join(os.getcwd(), '_cache'))
+    u = kwargs.get('username')
+    c = kwargs.get('client')
+    h = kwargs.get('headless')
+    self.driver = WhatsAPIDriver(client=c,
+                                 username=u,
+                                 profile=p,
+                                 headless=h)
     
-    except KeyError:
-      print("Please set the environment variable SELENIUM to Selenium URL")
-      sys.exit(1)
 
-    try:
-      client = os.environ['SELENIUM_CLIENT']
-      self.driver = WhatsAPIDriver(client=client,
-                                   username=username,
-                                   profile=profile,
-                                   headless=headless)
-    except KeyError:
-      print "Please set the environment variable SELENIUM_CLIENT to the client to be used"
-      sys.exit(1)
+  def setup(self, **kwargs):
+    for p in self.attributes:
+      m = self.getattribute(p)
+      m.setup(kwargs.get(p))
 
-    # Initialize ElasticSearch
-    self.es = Elasticsearch(config)
-    self.logfile = logfile
-    __log__ = "%s.json" % username
-    self.logpath = os.path.join(os.getcwd(), 'logs', __log__)
-    self.logfh = open(self.logpath, "a")
-    if (self.logfh.tell() == 0):
-      self.logfh.write('[]')
-      self.logfh.flush()
-
-  def run(self):
+  def run(self, **kwargs):
     while True:
-      time.sleep(0.1)
+      time.sleep(kwargs.frameTime)
       for message_group in self.driver.get_unread():
         for message in message_group.messages:
-          self.handle(message_group.chat, message)
+          for fn in kwargs.callbacks:
+            fn(message_group.chat, message)
 
-  def handle(self, chat, message):
-    self.log(chat, message)
-    result = self.es.search(index='memes', body={
-      'query': {
-        'match': {
-          'tag': message.content
-        }
-      }
-    })
-    hits = result['hits']['hits']
-    if len(hits) == 0:
-      chat.send_message('Não achei nenhum meme com essas palavras')
-      return
-    hits = hits[:3]
-    hits.reverse()
-    for meme in hits:
-      self.driver.send_media(meme['_source']['path'], chat.id, '')
+  def plugin(self, **kwargs):
+    try:
+      Repo.clone_from(kwargs.url, kwargs.path)
+      sys.path.append("%s" % kwargs.path)
+      module = __import__(kwargs.name)
+      className = getattr(module, kwargs.name.title())
+      instance = className(self.driver)
+      self.attributes.push(kwargs.name)
+      self.setattr(kwargs.name, instance)
+      print str(self.getattribute(kwargs.name))
+    except ImportError:
+      print "failed import %s" % p
 
+  def getattribute(self, key):
+    if key in self.attributes:
+      return self.properties[key]
+    else:
+      raise Exception("no %s attribute found")
 
-  def log(self, chat, message):
-    msg = {
-      'uid': chat.id,
-      'msg': message.content,
-      'tim': int(time.time()),
-    }
-    pos = self.logfh.tell()
-    self.logfh.seek(pos-1)
-    self.logfh.truncate()
-    if (pos > 2):
-      self.logfh.write(', ')
-      self.logfh.write(json.dumps(msg))
-      self.logfh.write(']')
-      self.logfh.flush()
+  def setattr(self, key, val):
+    if key in self.attributes:
+      self.properties[key] = val    
+    else:
+      raise Exception("no %s attribute defined")
--- lib/whatsapp.py
+++ lib/whatsapp.py
@@ -1,36 +1,73 @@
-# Imports
+# Import os and check for SELENIUM* environment variables
 import os
 import sys
-import argparse
 
-# Local imports
+# Initialize Driver (See for selenium url)
+try:
+  os.environ["SELENIUM"]  
+except KeyError:
+  print "Please set the environment variable SELENIUM to Selenium URL"
+  sys.exit(1)
+
+# Now imports
+import sys
+import time
+import os
+import argparse
+import json
 sys.path.append(os.getcwd())
 import bot
-
 
 # Arg parser
 parser = argparse.ArgumentParser()
 parser.add_argument('-u', '--username')
+parser.add_argument('-c', '--config')
+parser.add_argument('-C', '--client')
 parser.add_argument('-p', '--profile')
-parser.add_argument('-l', '--logfile')
-parser.add_argument('-H', '--elastic-search-host')
-parser.add_argument('-P', '--elastic-search-port')
+parser.add_argument('-H', '--headless', action='store_true')
 args = parser.parse_args()
 
-# Lets program
-username = args.username
-profile = ''
-if not args.profile:
-  profile = os.path.join(os.path.dirname(__file__), '..', '_cache') 
-else:
-  profile = args.profile
+# Implement a abstract bot,
+# with setup and run functions,
+# Like a Java Processing Art Framework
+class WebWhatsappWrapperBot(bot.Bot):
 
-# TODO add replicas
-elSearch = [
-  {
-    'host': args.elastic_search_host or 'localhost',
-    'port': args.elastic_search_port or '9200'
-  }
-]
-b = bot.Bot(username, profile, elSearch, args.logfile)
-b.run()
+  def __init__(self, **kwargs):
+    super(WebWhatsappWrapperBot, self).__init__(**kwargs)
+    
+    # Open and loads configuration for each
+    # plugin
+    self.config = {}
+    with open(kwargs.config) as json_data:
+      d = json.load(json_data)
+      for key,val in d.iteritems():
+        p = os.path.abspath(os.getcwd(), key)
+        self.plugin(name=key,
+                    url=val.url,
+                    path=p)
+        self.config[key] = val.config   
+
+  # A simple implementation of setup
+  # loaded form configuration readed by a json file
+  def setup(self):  
+    super.setup(self.config)
+    for key, val in self.config.iteritems():
+      print "%s: %s" % (key, val)
+
+  # A simple implementation of run with 0.1 seconds
+  # between a call and another call, with
+  # implementation of handle methods of each plugin
+  def run(self):
+    handles = [] 
+    for key, val in self.config.iteritems():
+      handle = self.__getattribute__(key).handle
+      handles.append(handle)
+    super.run(frameTime=0.1, callbacks=handles)
+      
+# Run bot
+www = WebWhatsappWrapperBot(client=args.client,
+                            username=args.username,
+                            config=args.config,
+                            headless=args.headless)
+www.setup()
+www.run()
